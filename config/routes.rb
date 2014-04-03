3cf849bc58f42185c02097e4d9bfc3a844f2b058
Rawley Greene <rawley69@gmail.com>
2014-04-02 20:16:46 -0700

    Got rid of pesky posts and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 SampleApp::Application.routes.draw do
-  resources :posts
-
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402055739) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
